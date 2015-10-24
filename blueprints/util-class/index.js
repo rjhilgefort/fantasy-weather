300ad07cfaca3e07fc5cb0c3fc49cfd952f8a000
Rob Hilgefort <rjhilgefort@gmail.com>
2015-10-24 14:44:01 -0400

    Add `util-class` blueprint

---
+++ blueprints/util-class/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  description: 'Generates a simple utility module ES6 class.'
+
+  // locals: function(options) {
+  //   // Return custom template variables here.
+  //   return {
+  //     foo: options.entity.options.foo
+  //   };
+  // }
+
+  // afterInstall: function(options) {
+  //   // Perform extra work here.
+  // }
+};
