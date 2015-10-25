b2c90875942d30f295661e4cdcb751858bb69339
Rob Hilgefort <rjhilgefort@gmail.com>
2015-10-25 13:27:57 -0400

    Moved NFL db

---
+++ app/utils/nfl-db.js
@@ -0,0 +1,149 @@
+export default [
+  {
+    name: "49ers",
+    locationIdentifier: "city",
+    stadium: {
+      city: "San Francisco",
+      state: "California",
+      name: "",
+      location: {
+        lat: 37.713486,
+        lng: -122.386256
+      },
+      roofType: "open"
+    }
+  },
+  {
+    name: "Bears",
+    locationIdentifier: "city",
+    stadium: {
+      identifier: "city",
+      city: "Chicago",
+      state: "Illinois",
+      name: "",
+      location: {
+        lat: 0,
+        lng: 0
+      },
+      roofType: "",
+    }
+  },
+  {
+    name: "Bengals",
+    locationIdentifier: "city",
+    stadium: {
+      city: "Cincinnati",
+      state: "Ohio",
+      location: {
+        lat: 39.095442,
+        lng: -84.516039
+      },
+      roofType: "open"
+    }
+  }/*,
+  {
+    name: "Bills",
+    locationName: "Buffalo"
+  },
+  {
+    name: "Broncos",
+    locationName: "Denver"
+  },
+  {
+    name: "Browns",
+    locationName: "Cleveland"
+  },
+  {
+    name: "Buccaneers",
+    locationName: "Tampa Bay"
+  },
+  {
+    name: "Chargers",
+    locationName: "San Diego"
+  },
+  {
+    name: "Colts",
+    locationName: "Indianapolis"
+  },
+  {
+    name: "Cowboys",
+    locationName: "Dallas"
+  },
+  {
+    name: "Dolphins",
+    locationName: "Miami"
+  },
+  {
+    name: "Falcons",
+    locationName: "Atlanta"
+  },
+  {
+    name: "Giants",
+    locationName: "New York"
+  },
+  {
+    name: "Jaguars",
+    locationName: "Jacksonville"
+  },
+  {
+    name: "Jets",
+    locationName: "New York"
+  },
+  {
+    name: "Lions",
+    locationName: "Detroit"
+  },
+  {
+    name: "Packers",
+    locationName: "Green Bay"
+  },
+  {
+    name: "Panthers",
+    locationName: "Carolina"
+  },
+  {
+    name: "Patriots",
+    locationName: "New England"
+  },
+  {
+    name: "Raiders",
+    locationName: "Oakland"
+  },
+  {
+    name: "Rams",
+    locationName: "St.Louis"
+  },
+  {
+    name: "Ravens",
+    locationName: "Baltimore"
+  },
+  {
+    name: "Redskins",
+    locationName: "Washington"
+  },
+  {
+    name: "Saints",
+    locationName: "New Orlean"
+  },
+  {
+    name: "Seahawks",
+    locationName: "Seattle"
+  },
+  {
+    name: "Steelers",
+    locationName: "Pittsburgh"
+  },
+  {
+    name: "Texans",
+    locationName: "Houston"
+  },
+  {
+    name: "Titans",
+    locationName: "Tennesse"
+  },
+  {
+    name: "Vikings",
+    locationName: "Minnesota"
+  }
+*/
+]
