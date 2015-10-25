e7ce70c2675f4dfeca65533b30ba1e975d218389
Rob Hilgefort <rjhilgefort@gmail.com>
2015-10-25 13:28:51 -0400

    initial build out of core object

--- app/models/_core/core-object.js
+++ app/models/_core/core-object.js
@@ -1,6 +1,39 @@
 import Ember from 'ember';
+import _ from 'lodash/lodash';
 
 let CoreObject = Ember.Object.extend({
+
+  init() {
+    this._super(...arguments);
+    this.defaultProperties(this._defaults());
+  },
+
+  _defaults() {
+    return {};
+  },
+
+  isBlank(keyName) {
+    return Ember.isBlank(this.get(keyName));
+  },
+
+  isPresent(keyName) {
+    return !this.isBlank(keyName);
+  },
+
+  default(keyName, value) {
+    if (this.isBlank(keyName)) {
+      this.set(keyName, value);
+    }
+    return value;
+  },
+
+  defaultProperties(hash) {
+    _.forEach(hash, (value, keyName) => {
+      this.default(keyName, value);
+    });
+    return hash;
+  }
+
 });
 
 CoreObject.reopenClass({
