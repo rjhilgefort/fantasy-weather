66aa786980aa8215ab42decb39e06cc2d7e3278a
Rob Hilgefort <rjhilgefort@gmail.com>
2015-10-27 23:41:18 -0400

    tested `nfl-schedule` service

--- app/pods/components/team-weather/component.js
+++ app/pods/components/team-weather/component.js
@@ -7,9 +7,10 @@ export default Ember.Component.extend({
 
   init() {
     this._super(...arguments);
-    let forecastio = this.get('forecastio');
+
     let location = this.get('team.stadium.location');
 
+    let forecastio = this.get('forecastio');
     forecastio.getLocationNow(location)
     // forecastio.getLocationTime(location, "2015-10-26T18:09:00-0400")
     // forecastio.getLocationTime(location, _.now())
@@ -18,9 +19,14 @@ export default Ember.Component.extend({
         this.set('weather', response);
         return response;
       });
+
+    let nflSchedule = this.get('nflSchedule');
+    console.log(nflSchedule.team(this.get('team.abbr')));
+    this.get('team').set('schedule', nflSchedule.team(this.get('team.abbr')));
   },
 
   forecastio: Ember.inject.service(),
+  nflSchedule: Ember.inject.service(),
 
   weather: null
 
