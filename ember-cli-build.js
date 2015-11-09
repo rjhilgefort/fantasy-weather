56384d88e8a6e323532d011cf992ed1aaecf747a
Rob Hilgefort <rjhilgefort@gmail.com>
2015-11-09 18:17:15 -0500

    working with lodash-extras locally

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -6,19 +6,6 @@ module.exports = function(defaults) {
     // Add options here
   });
 
-  // Use `app.import` to add additional libraries to the generated
-  // output files.
-  //
-  // If you need to use different assets in different
-  // environments, specify an object as the first parameter. That
-  // object's keys should be the environment name and the values
-  // should be the asset to use in that environment.
-  //
-  // If the library that you are including contains AMD or ES6
-  // modules that you would like to import into your application
-  // please specify an object with the list of modules as keys
-  // along with the exports of each module as its value.
-
   var importFont = function(font, options) {
     var extensions = ['eot', 'svg', 'ttf', 'woff', 'woff2'];
     extensions.forEach(function(extension) {
@@ -28,6 +15,9 @@ module.exports = function(defaults) {
 
   app.import(app.bowerDirectory + '/weather-icons/css/weather-icons.css');
   importFont(app.bowerDirectory + '/weather-icons/font/weathericons-regular-webfont',  { destDir: 'font' })
+
+  app.import(app.bowerDirectory + '/lodash/lodash.js');
+  app.import(app.bowerDirectory + '/lodash-extras/dist/lodash-extras.js');
 
   return app.toTree();
 };
