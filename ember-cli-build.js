3d952411d2636b89be54ba8f92ed0aff9d23f8ae
Rob Hilgefort <rjhilgefort@gmail.com>
2015-11-13 12:03:52 -0500

    Built out "ember-cli-import" utility, utilized in build

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,23 +1,18 @@
 /* global require, module */
 var EmberApp = require('ember-cli/lib/broccoli/ember-app');
+var CliImport = require('./build/ember-cli-import');
 
 module.exports = function(defaults) {
   var app = new EmberApp(defaults, {
     // Add options here
   });
+  var cliImport = new CliImport(app);
 
-  var importFont = function(font, options) {
-    var extensions = ['eot', 'svg', 'ttf', 'woff', 'woff2'];
-    extensions.forEach(function(extension) {
-      app.import(font + '.' + extension, options);
-    });
-  };
+  cliImport.bower('/weather-icons/css/weather-icons.css');
+  cliImport.bowerFont('/weather-icons/font/weathericons-regular-webfont',  { destDir: 'font' })
 
-  app.import(app.bowerDirectory + '/weather-icons/css/weather-icons.css');
-  importFont(app.bowerDirectory + '/weather-icons/font/weathericons-regular-webfont',  { destDir: 'font' })
-
-  app.import(app.bowerDirectory + '/lodash/lodash.js');
-  app.import(app.bowerDirectory + '/lodash-extras/dist/lodash-extras.js');
+  cliImport.bower('/lodash/lodash.js');
+  cliImport.bowerDevProd('/lodash-extras/dist/lodash-extras.js');
 
   return app.toTree();
 };
