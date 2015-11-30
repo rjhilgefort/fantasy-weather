49899c2ea87945c040f48fb326a697ffc5a6092f
Rob Hilgefort <rjhilgefort@gmail.com>
2015-11-29 19:52:36 -0500

    Added all the team info

--- app/data/nfl-db.js
+++ app/data/nfl-db.js
@@ -7,12 +7,16 @@ export default [
     stadium: {
       city: "San Francisco",
       state: "California",
-      name: "",
+      name: "Levi's Stadium",
       location: {
         lat: 37.713486,
         lng: -122.386256
       },
-      roofType: "open"
+      roofType: "open",
+      turf: {
+        type: "turf",
+        description: "Tifway II Bermuda Grass / Perennial Ryegrass mixture"
+      }
     }
   },
   {
@@ -23,12 +27,16 @@ export default [
     stadium: {
       city: "Chicago",
       state: "Illinois",
-      name: "",
+      name: "Soldier Field",
       location: {
         lat: 41.862306,
         lng: -87.616672
       },
-      roofType: "",
+      roofType: "open",
+      turf: {
+        type: "grass",
+        description: "Kentucky Bluegrass"
+      }
     }
   },
   {
@@ -39,116 +47,536 @@ export default [
     stadium: {
       city: "Cincinnati",
       state: "Ohio",
-      name: "",
+      name: "Paul Brown Stadium",
       location: {
         lat: 39.095442,
         lng: -84.516039
       },
-      roofType: "open"
+      roof: "open",
+      turf: {
+        type: "synthetic",
+        description: "UBU Speed Series S5-M Synthetic Turf"
+      }
     }
   },
   {
     name: "Bills",
-    locationName: "Buffalo"
+    abbreviation: "BUF",
+    logo: "",
+    locationIdentifier: "Buffalo",
+    stadium: {
+      city: "Orchard Park",
+      state: "New York",
+      name: "Ralph Wilson Stadium",
+      location: {
+        lat: 42.773739,
+        lng: -78.786978
+      },
+      roof: "open",
+      turf: {
+        type: "turf",
+        description: "A-Turf Titan"
+      }
+    }
   },
   {
     name: "Broncos",
-    locationName: "Denver"
+    abbreviation: "DEN",
+    logo: "",
+    locationIdentifier: "city",
+    stadium: {
+      city: "Denver",
+      state: "Colorado",
+      name: "Sports Authority Field at Mile High",
+      location: {
+        lat: 39.743936,
+        lng: -105.020097
+      },
+      roof: "open",
+      turf: {
+        type: "grass",
+        description: "Kentucky Bluegrass"
+      }
+    }
   },
   {
     name: "Browns",
-    locationName: "Cleveland"
+    abbreviation: "CLE",
+    logo: "",
+    locationIdentifier: "city",
+    stadium: {
+      city: "Cleveland",
+      state: "Ohio",
+      name: "FirstEnergy Stadium",
+      location: {
+        lat: 41.506022,
+        lng: -81.699564
+      },
+      roof: "open",
+      turf: {
+        type: "grass",
+        description: "Kentucky Bluegrass"
+      }
+    }
   },
   {
     name: "Buccaneers",
-    locationName: "Tampa Bay"
+    abbreviation: "TB",
+    logo: "",
+    locationIdentifier: "Tampa Bay",
+    stadium: {
+      city: "Tampa",
+      state: "Florida",
+      name: "Raymond James Stadium",
+      location: {
+        lat: 27.975967,
+        lng: -82.50335
+      },
+      roof: "open",
+      turf: {
+        type: "grass",
+        description: "Tifway 419 Bermuda Grass"
+      }
+    }
   },
   {
     name: "Chargers",
-    locationName: "San Diego"
+    abbreviation: "SD",
+    logo: "",
+    locationIdentifier: "city",
+    stadium: {
+      city: "San Diego",
+      state: "California",
+      name: "Qualcomm Stadium",
+      location: {
+        lat: 32.783117,
+        lng: -117.119525
+      },
+      roof: "open",
+      turf: {
+        type: "grass",
+        description: "Bandera Bermuda Grass"
+      }
+    }
   },
   {
     name: "Colts",
-    locationName: "Indianapolis"
+    abbreviation: "IND",
+    logo: "",
+    locationIdentifier: "city",
+    stadium: {
+      city: "Indianapolis",
+      state: "Indiana",
+      name: "Lucas Oil Stadium",
+      location: {
+        lat: 39.760056,
+        lng: -86.163806
+      },
+      roof: "retractable",
+      turf: {
+        type: "turf",
+        description: "FieldTurf Classic HD"
+      }
+    }
   },
   {
     name: "Cowboys",
-    locationName: "Dallas"
+    abbreviation: "DAL",
+    logo: "",
+    locationIdentifier: "Dallas",
+    stadium: {
+      city: "Arlington",
+      state: "Texas",
+      name: "AT&T Stadium",
+      location: {
+        lat: 32.747778,
+        lng: -97.092778
+      },
+      roof: "retractable",
+      turf: {
+        type: "turf",
+        description: "Matrix RealGrass Artificial Turf"
+      }
+    }
   },
   {
     name: "Dolphins",
-    locationName: "Miami"
+    abbreviation: "MIA",
+    logo: "",
+    locationIdentifier: "Miami",
+    stadium: {
+      city: "Miami Gardens",
+      state: "Florida",
+      name: "Sun Life Stadium",
+      location: {
+        lat: 25.957919,
+        lng: -80.238842
+      },
+      roof: "open",
+      turf: {
+        type: "turf",
+        description: "Prescription Athletic Turf (Tifway 419 Bermuda Grass)"
+      }
+    }
   },
   {
     name: "Falcons",
-    locationName: "Atlanta"
+    abbreviation: "ATL",
+    logo: "",
+    locationIdentifier: "city",
+    stadium: {
+      city: "Atlanta",
+      state: "Georgia",
+      name: "Georgia Dome",
+      location: {
+        lat: 33.757614,
+        lng: -84.400972
+      },
+      roof: "dome",
+      turf: {
+        type: "turf",
+        description: "FieldTurf Classic HD"
+      }
+    }
   },
   {
     name: "Giants",
-    locationName: "New York"
+    abbreviation: "NYG",
+    logo: "",
+    locationIdentifier: "New York",
+    stadium: {
+      city: "East Rutherford",
+      state: "New Jersey",
+      name: "MetLife Stadium",
+      location: {
+        lat: 40.812194,
+        lng: -74.076983
+      },
+      roof: "open",
+      turf: {
+        type: "turf",
+        description: "UBU Speed Series S5-M Synthetic Turf"
+      }
+    }
   },
   {
     name: "Jaguars",
-    locationName: "Jacksonville"
+    abbreviation: "JAC",
+    logo: "",
+    locationIdentifier: "city",
+    stadium: {
+      city: "Jacksonville",
+      state: "Florida",
+      name: "EverBank Field",
+      location: {
+        lat: 30.323925,
+        lng: -81.637356
+      },
+      roof: "open",
+      turf: {
+        type: "grass",
+        description: "Tifway 419 Bermuda Grass"
+      }
+    }
   },
   {
     name: "Jets",
-    locationName: "New York"
+    abbreviation: "NYJ",
+    logo: "",
+    locationIdentifier: "New York",
+    stadium: {
+      city: "East Rutherford",
+      state: "New Jersey",
+      name: "MetLife Stadium",
+      location: {
+        lat: 40.812194,
+        lng: -74.076983
+      },
+      roof: "open",
+      turf: {
+        type: "turf",
+        description: "UBU Speed Series S5-M Synthetic Turf"
+      }
+    }
   },
   {
     name: "Lions",
-    locationName: "Detroit"
+    abbreviation: "DET",
+    logo: "",
+    locationIdentifier: "city",
+    stadium: {
+      city: "Detroit",
+      state: "Michigan",
+      name: "Ford Field",
+      location: {
+        lat: 42.340156,
+        lng: -83.045808
+      },
+      roof: "dome",
+      turf: {
+        type: "turf",
+        description: "FieldTurf Classic HD"
+      }
+    }
   },
   {
     name: "Packers",
-    locationName: "Green Bay"
+    abbreviation: "GB",
+    logo: "",
+    locationIdentifier: "city",
+    stadium: {
+      city: "Green Bay",
+      state: "Wisconsin",
+      name: "Lambeau Field",
+      location: {
+        lat: 44.501306,
+        lng: -88.062167
+      },
+      roof: "open",
+      turf: {
+        type: "grass",
+        description: "Desso GrassMaster"
+      }
+    }
   },
   {
     name: "Panthers",
-    locationName: "Carolina"
+    abbreviation: "CAR",
+    logo: "",
+    locationIdentifier: "Carolina",
+    stadium: {
+      city: "Charlotte",
+      state: "North Carolina",
+      name: "Bank of America Stadium",
+      location: {
+        lat: 35.225808,
+        lng: -80.852861
+      },
+      roof: "open",
+      turf: {
+        type: "grass",
+        description: "Voyager Bermuda Grass"
+      }
+    }
   },
   {
     name: "Patriots",
-    locationName: "New England"
+    abbreviation: "NE",
+    logo: "",
+    locationIdentifier: "New England",
+    stadium: {
+      city: "Foxborough",
+      state: "Massachusetts",
+      name: "Gillette Stadium",
+      location: {
+        lat: 42.090925,
+        lng: -71.26435
+      },
+      roof: "open",
+      turf: {
+        type: "turf",
+        description: "FieldTurf Revolution"
+      }
+    }
   },
   {
     name: "Raiders",
-    locationName: "Oakland"
+    abbreviation: "OAK",
+    logo: "",
+    locationIdentifier: "city",
+    stadium: {
+      city: "Oakland",
+      state: "California",
+      name: "O.co Coliseum",
+      location: {
+        lat: 37.751411,
+        lng: -122.200889
+      },
+      roof: "open",
+      turf: {
+        type: "grass",
+        description: "Tifway II Bermuda Grass"
+      }
+    }
   },
   {
     name: "Rams",
-    locationName: "St.Louis"
+    abbreviation: "STL",
+    logo: "",
+    locationIdentifier: "city",
+    stadium: {
+      city: "St. Louis",
+      state: "Missouri",
+      name: "Edward Jones Dome",
+      location: {
+        lat: 38.632975,
+        lng: -90.188547
+      },
+      roof: "dome",
+      turf: {
+        type: "turf",
+        description: "AstroTurf GameDay Grass 3D"
+      }
+    }
   },
   {
     name: "Ravens",
-    locationName: "Baltimore"
+    abbreviation: "BAL",
+    logo: "",
+    locationIdentifier: "city",
+    stadium: {
+      city: "Baltimore",
+      state: "Maryland",
+      name: "M&T Bank Stadium",
+      location: {
+        lat: 39.277969,
+        lng: -76.622767
+      },
+      roof: "open",
+      turf: {
+        type: "turf",
+        description: "Shaw Sports Momentum 51"
+      }
+    }
   },
   {
     name: "Redskins",
-    locationName: "Washington"
+    abbreviation: "WAS",
+    logo: "",
+    locationIdentifier: "Washington",
+    stadium: {
+      city: "Landover",
+      state: "Maryland",
+      name: "FedEx Field",
+      location: {
+        lat: 38.907697,
+        lng: -76.864517
+      },
+      roof: "open",
+      turf: {
+        type: "grass",
+        description: "Latitude 36 Bermuda Grass"
+      }
+    }
   },
   {
     name: "Saints",
-    locationName: "New Orlean"
+    abbreviation: "NO",
+    logo: "",
+    locationIdentifier: "city",
+    stadium: {
+      city: "New Orleans",
+      state: "Louisiana",
+      name: "Mercedes-Benz Superdome",
+      location: {
+        lat: 29.950931,
+        lng: -90.081364
+      },
+      roof: "dome",
+      turf: {
+        type: "turf",
+        description: "UBU Speed Series S5-M Synthetic Turf"
+      }
+    }
   },
   {
     name: "Seahawks",
-    locationName: "Seattle"
+    abbreviation: "SEA",
+    logo: "",
+    locationIdentifier: "city",
+    stadium: {
+      city: "Seattle",
+      state: "Washington",
+      name: "CenturyLink Field",
+      location: {
+        lat: 47.595153,
+        lng: -122.331625
+      },
+      roof: "open",
+      turf: {
+        type: "turf",
+        description: "FieldTurf Revolution"
+      }
+    }
   },
   {
     name: "Steelers",
-    locationName: "Pittsburgh"
+    abbreviation: "PIT",
+    logo: "",
+    locationIdentifier: "city",
+    stadium: {
+      city: "Pittsburgh",
+      state: "Pennsylvania",
+      name: "Heinz Field",
+      location: {
+        lat: 40.446786,
+        lng: -80.015761
+      },
+      roof: "open",
+      turf: {
+        type: "grass",
+        description: "Kentucky Bluegrass"
+      }
+    }
   },
   {
     name: "Texans",
-    locationName: "Houston"
+    abbreviation: "HOU",
+    logo: "",
+    locationIdentifier: "city",
+    stadium: {
+      city: "Houston",
+      state: "Texas",
+      name: "NRG Stadium",
+      location: {
+        lat: 29.684781,
+        lng: -95.410956
+      },
+      roof: "retractable",
+      turf: {
+        type: "turf",
+        description: "AstroTurf GameDay Grass 3D"
+      }
+    }
   },
   {
     name: "Titans",
-    locationName: "Tennesse"
+    abbreviation: "TEN",
+    logo: "",
+    locationIdentifier: "state",
+    stadium: {
+      city: "Nashville",
+      state: "Tennessee",
+      name: "Nissan Stadium",
+      location: {
+        lat: 36.166461,
+        lng: -86.771289
+      },
+      roof: "open",
+      turf: {
+        type: "grass",
+        description: "TifSport Bermuda Grass"
+      }
+    }
   },
   {
     name: "Vikings",
-    locationName: "Minnesota"
+    abbreviation: "MIN",
+    logo: "",
+    locationIdentifier: "state",
+    stadium: {
+      city: "Minneapolis",
+      state: "Minnesota",
+      name: "TCF Bank Stadium",
+      location: {
+        lat: 44.973881,
+        lng: -93.258094
+      },
+      roof: "open",
+      turf: {
+        type: "turf",
+        description: "FieldTurf Revolution"
+      }
+    }
   }
 ];
