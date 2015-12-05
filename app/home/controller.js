2ac30a78c47fdace96329f22ca514940854d52d6
Rob Hilgefort <rjhilgefort@gmail.com>
2015-12-04 22:32:25 -0500

    Built out better page title

--- app/home/controller.js
+++ app/home/controller.js
@@ -1,4 +1,21 @@
 import Ember from 'ember';
+import computed from 'ember-computed-decorators';
 
 export default Ember.Controller.extend({
+
+  @computed('weekCurrent')
+  firstGame(weekCurrent) {
+    return _.first(weekCurrent);
+  },
+
+  @computed('firstGame')
+  weekNumber(firstGame) {
+    return firstGame.week;
+  },
+
+  @computed('firstGame')
+  weekStart(firstGame) {
+    return `${firstGame.month}/${firstGame.day}/${firstGame.year}`;
+  }
+
 });
